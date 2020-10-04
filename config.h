ca4564be34fae1d6ebacc837f1098e806ed76297
klorophatu <klorophatu@krutt.org>
2020-10-04 10:25:24 +0300

    Change color scheme

--- config.h
+++ config.h
@@ -83,42 +83,44 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 1;
 
 
 /* Terminal colors (16 first used in escape sequence) */
-const char *colorname[] = {
-  "#3b4252", /*  0: nord1 */
-  "#bf616a", /*  1: nord11 */
-  "#a3be8c", /*  2: nord14 */
-  "#ebcb8b", /*  3: nord13 */
-  "#81a1c1", /*  4: nord9 */
-  "#b48ead", /*  5: nord15 */
-  "#88c0d0", /*  6: nord8 */
-  "#e5e9f0", /*  7: nord5 */
-  "#4c566a", /*  8: nord3 */
-  "#bf616a", /*  9: nord11 */
-  "#a3be8c", /* 10: nord14 */
-  "#ebcb8b", /* 11: nord3 */
-  "#81a1c1", /* 12: nord9 */
-  "#b48ead", /* 13: nord15 */
-  "#8fbcbb", /* 14: nord7 */
-  "#eceff4", /* 15: nord6 */
-  [255] = 0,
-  // defaults
-  [256] = "#d8dee9", /* 256: nord4 */
-  [257] = "#2e3440", /* 257: nord0 */
+ static const char *colorname[] = {
+  /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bbbbbb", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#44475a", /* black   */
+  [9]  = "#ff5555", /* red     */
+  [10] = "#50fa7b", /* green   */
+  [11] = "#f1fa8c", /* yellow  */
+  [12] = "#bd93f9", /* blue    */
+  [13] = "#ff79c6", /* magenta */
+  [14] = "#8be9fd", /* cyan    */
+  [15] = "#ffffff", /* white   */
+
+  /* special colors */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
 };
 
-/*
-* Default colors (colorname index)
-* foreground, background, cursor, reverse
-* cursor
-*/
-unsigned int defaultfg = 256; /* nord4 */
-unsigned int defaultbg = 257; /* nord0 */
-unsigned int defaultcs = 256; /* nord4 */
-unsigned int defaultrcs = 8;  /* nord3 */
+ /*
+  * Default colors (colorname index)
+  * foreground, background, cursor
+  */
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 257;
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
  * will reverse too. Another logic would only make the simple feature too
